c18e710eb4be0a3c4f2df9cc221e218077e3407b
mark loktionov <markloktionov77@gmail.com>
2023-09-12 14:20:12 +0400

    feat: add drop-menu

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -4,11 +4,16 @@ let navBar = document.querySelector('.nav-bar');
 let body = document.querySelector('.body')
 let navItem = document.querySelectorAll('.nav-li')
 
+import { menuNoAuth, menuAuth } from "./drop-menu.js";
+
 burger.addEventListener('click', () => {
     burger.classList.toggle('rotate-pad')
     navBar.classList.toggle('nav-none');
     burgerElem.classList.toggle('rotate')
     body.classList.toggle('non-scroll')
+
+    menuAuth.classList.add('none');
+    menuNoAuth.classList.add('none'); // close drop-menu
 })
 
 for (let i = 0; i < navItem.length; i++) {
@@ -19,3 +24,5 @@ for (let i = 0; i < navItem.length; i++) {
         body.classList.toggle('non-scroll')
     })
 }
+
+export {burger, burgerElem, body, navBar};
